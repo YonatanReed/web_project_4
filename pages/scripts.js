008a9efd20c44ebec186c1cae05cebf693e6cf84
YonatanReed <98915271+YonatanReed@users.noreply.github.com>
2022-03-31 11:20:59 +0300

    refactor: fixed BEM classes

--- pages/scripts.js
+++ pages/scripts.js
@@ -1,6 +1,6 @@
 const openFormButton = document.querySelector(".profile__edit-btn");
 const popup = document.querySelector(".popup-box");
-const closeButton = popup.querySelector(".popup__close-btn");
+const closeButton = popup.querySelector(".popup-box__close-btn");
 
 function toggleForm() {
   popup.classList.toggle("popup-box_opened");
@@ -16,8 +16,8 @@ const profileTitle = document.querySelector(".profile__job");
 
 function handleProfileFormSubmit(event) {
   event.preventDefault();
-  const nameInput = document.querySelector(".popup__input_type_name");
-  const jobInput = document.querySelector(".popup__input_type_job");
+  const nameInput = document.querySelector(".form__input_type_name");
+  const jobInput = document.querySelector(".form__input_type_job");
 
   profileName.textContent = nameInput.value;
   profileTitle.textContent = jobInput.value;
